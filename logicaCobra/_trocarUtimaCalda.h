53ca39770a88894c098e069a2feb3ec24e683921
Kaua Hen <kaua.da@ufpi.edu.br>
2024-02-09 02:16:31 -0300

    Refactor trocarUtimaCalda function

--- logicaCobra/_trocarUtimaCalda.h
+++ logicaCobra/_trocarUtimaCalda.h
@@ -8,13 +8,16 @@ void trocarUtimaCalda()
   int caldaAnteriorX, caldaAnteriorY;
   caldaAnteriorX = dadosCabeca.cabecaX;
   caldaAnteriorY = dadosCabeca.cabecaY;
+
   for (int i = 0; i < dadosCalda.tamanhoCauda; i++)
   {
     int caldaAnteriorSegX, caldaAnteriorSegY;
     caldaAnteriorSegX = caldaAnteriorX;
     caldaAnteriorSegY = caldaAnteriorY;
+
     caldaAnteriorX = dadosCalda.caldaX[i];
     caldaAnteriorY = dadosCalda.caldaY[i];
+
     dadosCalda.caldaX[i] = caldaAnteriorSegX;
     dadosCalda.caldaY[i] = caldaAnteriorSegY;
   }
